464f7a74a6edbf8604b5c35d44e2071420e95179
Darice <29387736+dguittet@users.noreply.github.com>
2017-09-19 11:23:33 -0600

    windpower_test default case setup

--- test/ssc_test/cmod_windpower_test.cpp
+++ test/ssc_test/cmod_windpower_test.cpp
@@ -4,16 +4,11 @@
 #include "vartab.h"
 #include "common.h"
 #include "cmod_windpower.h"
+#include "cmod_windpower_test.h"
 
-class CMWindPowerTest : public ::testing::Test{
-private:
-	void assign_default_variables(var_table* vt);
-	void create_default();
-
-protected:
-	cm_windpower* cm;
-};
 
 TEST_F(CMWindPowerTest, IntegrationTest_cmod_windpower){
 	cm = new cm_windpower();
+	//create_default();
+	
 }
